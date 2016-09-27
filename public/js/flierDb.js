d782697b3c8d469a49580e56a337122c4fc02e63
Avi Kejariwal <avikejariwal@Avis-MacBook-Pro.local>
2016-09-26 18:44:04 -0700

    Removed Materialize carousel. Using simple JS

--- public/js/flierDb.js
+++ public/js/flierDb.js
@@ -1,15 +1,31 @@
+var i = 0;
 
 $(document).ready(function(){
-      $('.carousel.carousel-slider').carousel({full_width: true, time_constant:500});
-
+	var oneImage = false;
+	$('#carousel img').each(function(){
+		if (oneImage){
+			$(this).hide();
+		}
+		else {
+			$(this).show();
+			oneImage = true;
+		}
+	})
       window.setInterval(function(){
+      	console.log("Next slide");
       	nextSlide();
-      }, 10000);
+      }, 5000);
     });
 
 function nextSlide(){
-	var numFliers = $('#flierCarousel a').length;
-	if (numFliers != 1){
-		$('.carousel.carousel-slider').carousel('next');
-	}	
+	var numFliers = $("#carousel img").length;
+	var j = 0;
+	$('#carousel img').each(function(){
+		$(this).hide();
+		if (i == j){
+			$(this).show();
+		}
+		j = (j + 1) % numFliers;
+	})
+	i = (i + 1) % numFliers;
 }
